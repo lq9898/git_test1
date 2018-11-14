f689a2c21bce50603e5e5a5891851bad6e2844d5
lq <1173122910@qq.com>
2018-11-14 20:46:25 +0800

    aaa

--- src/main/java/com/lq/controller/TestGit.java
+++ src/main/java/com/lq/controller/TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("i am zhangsan");
         System.out.println("i am lisi");
+        System.out.println("my name is xiaomingming");
 
     }
 }
