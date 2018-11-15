e4972b97c6e08554754e876467d9f42ac4e58c11
lq <1173122910@qq.com>
2018-11-15 09:34:32 +0800

    测试a

--- src/test/java/com/baizhi/test/EmpTeste.java
+++ src/test/java/com/baizhi/test/EmpTeste.java
@@ -1,7 +1,9 @@
 package com.baizhi.test;
 
 import com.lq.entity.Emp;
+import com.lq.entity.User;
 import com.lq.service.EmpService;
+import com.lq.service.UserService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,6 +12,8 @@ import java.util.List;
 public class EmpTeste extends AbTest {
     @Autowired
     private EmpService empService;
+    @Autowired
+    private UserService userService;
 
     @Test
     public void queryAll() {
@@ -17,5 +21,15 @@ public class EmpTeste extends AbTest {
         for (Emp emp : emps) {
             System.out.println(emp);
         }
+    }
+
+    @Test
+    public void insert() {
+        User user = new User();
+        user.setRealname("leiqin");
+        user.setRealname("lq");
+        user.setPassword("123456");
+        user.setSex("nv");
+        userService.register(user);
     }
 }
